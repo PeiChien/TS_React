edd9db12c977b5f51103ba904707945abf8d8087
PeiChien <925846753@qq.com>
2019-03-28 22:03:20 +0800

    Create Footer.tsx

---
+++ src/components/Footer.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../css/Footer.css';
+class Footer extends React.Component{
+    render(){
+        return (
+            <footer className="navbar-inverse text-center">
+                <div id="copyrights">PeiChien © 2019.All Rights Reversed</div>
+            </footer>
+        )
+    }
+}
+export {Footer};
